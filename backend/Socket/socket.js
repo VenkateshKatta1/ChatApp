6b3331804c399b43565104300ba10b27c2350d9f
VenkateshKatta1 <venkateshkatta5@gmail.com>
2024-10-21 12:07:46 -0500

    #

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -5,12 +5,20 @@ import express from "express";
 const app = express();
 
 const server = http.createServer(app);
+const allowedOrigins = [
+  "http://localhost:5173", // Dev URL
+  "https://chatapp-grib.onrender.com", // Production URL
+];
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
-    credentials: true,
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
   },
+  methods: ["GET", "POST"],
+  credentials: true,
 });
 
 export const getReceiverSocketId = (receiverId) => {
